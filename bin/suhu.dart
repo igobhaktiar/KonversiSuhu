8a308ce07274b33c9210ff343ae5e1bc8d1c255d
Firgo Bhaktiar Hamsah <71805833+igobhaktiar@users.noreply.github.com>
2023-07-10 20:45:15 +0700

    suhu celcius

--- bin/suhu.dart
+++ bin/suhu.dart
@@ -3,8 +3,18 @@ import 'dart:io';
 
 void main(List<String> arguments) {
   stdout.write('Masukkan Suhu dalam Farenheit: ');
-  var farenheit = num.parse(stdin.readLineSync()!);
+  var suhufarenheit = num.parse(stdin.readLineSync()!);
 
-  var celcius = (farenheit -32 ) * 5/9;
-  print('$farenheit derajat Farenheit = $celcius derajat celcius');
+  stdout.write('Masukkan Suhu dalam celcius: ');
+  var suhuCelcius = num.parse(stdin.readLineSync()!);
+
+  var celcius = (suhufarenheit -32 ) * 5/9;
+  var reamur = (suhufarenheit - 32 ) * 4/9;
+  var kelvin = (suhufarenheit - 32) * 5/9 + 273;
+  var farenheit = (suhuCelcius + 32) * 9/5;
+
+  print('$suhufarenheit derajat Farenheit = $celcius derajat celcius');
+  print('$suhufarenheit derajat Farenheit = $reamur derajat reamur');
+  print('$suhufarenheit derajat Farenheit = $kelvin derajat kelvin');
+  print('$suhuCelcius derajat celcius = $farenheit derajat farenheit');
 }
